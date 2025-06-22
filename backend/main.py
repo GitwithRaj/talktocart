2323f0b752b829eea85665a110d4b188463cc76b
Raj Kumar Prajapati <2020itb043.raj@students.iiests.ac.in>
2025-06-22 22:40:53 +0530

    requirement.txt added

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app = FastAPI()
 # Allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
